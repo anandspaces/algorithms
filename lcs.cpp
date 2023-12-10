b719d49f600a7f8378c4b0f8f01e5b9c3c1eec67
pantherasapiens <116384089+pantherasapiens@users.noreply.github.com>
2023-12-10 18:54:48 +0530

    travelling salesman problem, 0/1 knapsack problem, longenst common subsequence

---
+++ lcs.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Function to find the length of the Longest Common Subsequence
+int lcsLength(const string& X, const string& Y, int m, int n, int dp[100][100]) {
+    for (int i = 0; i <= m; ++i) {
+        for (int j = 0; j <= n; ++j) {
+            if (i == 0 || j == 0) {
+                dp[i][j] = 0;
+            } else if (X[i - 1] == Y[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+// Function to find the Longest Common Subsequence
+void printLCS(const string& X, const string& Y, int m, int n, const int dp[100][100]) {
+    int index = dp[m][n];
+    string lcs(index, ' ');
+
+    int i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (X[i - 1] == Y[j - 1]) {
+            lcs[--index] = X[i - 1];
+            --i;
+            --j;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            --i;
+        } else {
+            --j;
+        }
+    }
+
+    cout << "Longest Common Subsequence: " << lcs << endl;
+}
+
+int main() {
+    const string X = "ABCBDAB";
+    const string Y = "BDCAB";
+    int m = X.length();
+    int n = Y.length();
+
+    int dp[100][100];
+
+    int length = lcsLength(X, Y, m, n, dp);
+
+    cout << "Length of Longest Common Subsequence: " << length << endl;
+
+    printLCS(X, Y, m, n, dp);
+
+    return 0;
+}
---
+++ tsp.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+const int INF = 1e9;
+
+int tsp(int mask, int pos, int dist[4][4], int dp[16][4], int n) {
+    // If all cities have been visited
+    if (mask == (1 << n) - 1) {
+        return dist[pos][0]; // Return to the starting city
+    }
+
+    // If this subproblem has already been solved
+    if (dp[mask][pos] != -1) {
+        return dp[mask][pos];
+    }
+
+    int ans = INF;
+
+    // Try visiting each unvisited city
+    for (int city = 0; city < n; ++city) {
+        if ((mask & (1 << city)) == 0) { // Check if the city is unvisited
+            int newDist = dist[pos][city] + tsp(mask | (1 << city), city, dist, dp, n);
+            ans = min(ans, newDist);
+        }
+    }
+
+    return dp[mask][pos] = ans;
+}
+
+int main() {
+    // Example data
+    int n = 4; // Number of cities
+
+    // Distance matrix (dist[i][j] represents the distance from city i to city j)
+    int dist[4][4] = {
+        {0, 10, 15, 20},
+        {10, 0, 35, 25},
+        {15, 35, 0, 30},
+        {20, 25, 30, 0}
+    };
+
+    // Initialize DP memoization table
+    int dp[16][4];
+    for (int i = 0; i < (1 << n); ++i) {
+        for (int j = 0; j < n; ++j) {
+            dp[i][j] = -1;
+        }
+    }
+
+    // Solve the TSP
+    int minCost = tsp(1, 0, dist, dp, n); // Start from city 0, with the first city visited
+
+    cout << "Minimum cost of TSP: " << minCost << endl;
+
+    return 0;
+}
